903f92be5df22285f39da3b44acf04a00f41e5ba
tokarev_pa <tokarev_pa@tkbbank.ru>
2024-02-21 18:06:35 +0300

    Data: добавил контейнер бд для локального запуска

--- data/src/test/java/com/github/rusichpt/data/TestDataApplication.java
+++ data/src/test/java/com/github/rusichpt/data/TestDataApplication.java
@@ -2,6 +2,9 @@ package com.github.rusichpt.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestDataApplication {
@@ -10,4 +13,9 @@ public class TestDataApplication {
         SpringApplication.from(DataApplication::main).with(TestDataApplication.class).run(args);
     }
 
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:14.1");
+    }
 }
