9ae3bd1e9d643fb7ff0768ebfd76fb1686c4bc08
tokarev_pa <tokarev_pa@tkbbank.ru>
2024-03-18 17:17:57 +0300

    Docker compose: добавил стартер

--- docker-compose/src/main/java/com/github/rusichpt/dockercompose/DockerComposeApplication.java
+++ docker-compose/src/main/java/com/github/rusichpt/dockercompose/DockerComposeApplication.java
@@ -2,18 +2,12 @@ package com.github.rusichpt.dockercompose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.github.rusichpt")
-@EnableJpaRepositories(basePackages = "com.github.rusichpt.data")
-@EntityScan(basePackages = "com.github.rusichpt.data")
 public class DockerComposeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerComposeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeApplication.class, args);
+    }
 
 }
--- docker-compose/src/main/resources/data.sql
+++ docker-compose/src/main/resources/data.sql
@@ -6,3 +6,12 @@ INSERT INTO public.users (age, about, "name")
 VALUES (18, 'С# developer', 'Дмитрий');
 INSERT INTO public.users (age, about, "name")
 VALUES (25, 'Python developer', 'Oлег');
+
+INSERT INTO public.products ("name", price)
+VALUES ('Xiaomi Смартфон Redmi', 4898);
+INSERT INTO public.products ("name", price)
+VALUES ('realme Смартфон Note 50', 4703);
+INSERT INTO public.products ("name", price)
+VALUES ('Infinix Смартфон GT 10 Pro', 27779);
+INSERT INTO public.products ("name", price)
+VALUES ('Tecno Смартфон Spark 20 Pro+', 18303);
