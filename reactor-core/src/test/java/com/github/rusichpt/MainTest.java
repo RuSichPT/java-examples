e2865c1197af4e8fec4c3b9ebbc5c53b43222a74
tokarev_pa <tokarev_pa@tkbbank.ru>
2024-04-05 17:08:50 +0300

    Generics: дополнил тестами

--- reactor-core/src/test/java/com/github/rusichpt/MainTest.java
+++ reactor-core/src/test/java/com/github/rusichpt/MainTest.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +19,6 @@ class MainTest {
     @Test
     void test1() {
         List<Integer> elements = new ArrayList<>();
-
         Flux.just(1, 2, 3, 4)
                 .log()
                 .subscribe(elements::add);
@@ -27,7 +29,6 @@ class MainTest {
     @Test
     void test2() {
         List<Integer> elements = new ArrayList<>();
-
         Flux.just(1, 2, 3, 4)
                 .log()
                 .subscribe(new Subscriber<Integer>() {
@@ -56,7 +57,6 @@ class MainTest {
     @Test
     void test3() {
         List<Integer> elements = new ArrayList<>();
-
         Flux.just(1, 2, 3, 4)
                 .log()
                 .subscribe(new Subscriber<Integer>() {
@@ -80,5 +80,99 @@ class MainTest {
                 });
 
         assertThat(elements).containsExactly(1, 2);
+    }
+    @Test
+    void test4() {
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription s;
+                    int onNextAmount;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        this.s = s;
+                        s.request(2);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        elements.add(integer);
+                        onNextAmount++;
+                        if (onNextAmount % 2 == 0) {
+                            s.request(2);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {}
+
+                    @Override
+                    public void onComplete() {}
+                });
+
+        assertThat(elements).containsExactly(1, 2, 3, 4);
+    }
+
+    @Test
+    void test5() {
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> {
+                    log.info("{}:{}", i, Thread.currentThread());
+                    return i * 2;
+                })
+                .subscribe(elements::add);
+        assertThat(elements).containsExactly(2, 4, 6, 8);
+    }
+
+    @Test
+    void test6() {
+        List<String> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .zipWith(Flux.range(0, Integer.MAX_VALUE)
+                                .log(),
+                        (one, two) -> String.format("First Flux: %d, Second Flux: %d", one, two))
+                .log()
+                .subscribe(elements::add);
+
+        assertThat(elements).containsExactly(
+                "First Flux: 2, Second Flux: 0",
+                "First Flux: 4, Second Flux: 1",
+                "First Flux: 6, Second Flux: 2",
+                "First Flux: 8, Second Flux: 3");
+    }
+
+    @Test
+    void test7() {
+        ConnectableFlux<Object> publish = Flux.create(fluxSink -> {
+                    while(true) {
+                        fluxSink.next(System.currentTimeMillis());
+                    }
+                })
+                .sample(Duration.ofSeconds(2))
+                .publish();
+
+        publish.subscribe(o-> System.out.println(o + " consumer1"));
+        publish.subscribe(o-> System.out.println(o + " consumer2"));
+
+//        publish.connect();
+    }
+
+    @Test
+    void test8() {
+        log.info("start");
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(elements::add);
+
+        assertThat(elements).containsExactly(2, 4, 6, 8);
     }
 }
